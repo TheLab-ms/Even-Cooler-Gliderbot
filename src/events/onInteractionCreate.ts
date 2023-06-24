d045ba142bd9c1505ed989de9838cb649764ed30
Cameron Steele <cam.steeleis@gmail.com>
2023-06-24 01:33:58 -0500

    Added support for buttons

--- src/events/onInteractionCreate.ts
+++ src/events/onInteractionCreate.ts
@@ -5,17 +5,27 @@ export default async function onInteractionCreate(
   interaction: Interaction<CacheType>,
   data: EventData,
 ) {
-  if (!interaction.isCommand()) return;
-  const { commands } = data;
-  const command = commands.get(interaction.commandName);
-  if (!command) return;
-  try {
-    await interaction.deferReply({ ephemeral: command.isEphemeral });
-    await command.run(interaction);
-  } catch (error) {
-    console.error(error);
+  if (interaction.isCommand()) {
+    const { commands } = data;
+    const command = commands.get(interaction.commandName);
+    if (!command) return;
+    try {
+      await interaction.deferReply({ ephemeral: command.isEphemeral });
+      await command.run(interaction);
+    } catch (error) {
+      console.error(error);
+      await interaction.editReply({
+        content: 'There was an error while executing this command!',
+      });
+    }
+    return;
+  }
+
+  if (interaction.isButton()) {
+    await interaction.deferReply({ ephemeral: true });
     await interaction.editReply({
-      content: 'There was an error while executing this command!',
+      content: 'This button has not been implemented yet!',
     });
+    return;
   }
 }
