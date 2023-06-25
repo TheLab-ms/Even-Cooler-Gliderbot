b2791a441ca68488a4b62e78421872a961da0564
Cameron Steele <cam.steeleis@gmail.com>
2023-06-24 21:00:03 -0500

    Add account command

--- src/helpers/loadCommands.ts
+++ src/helpers/loadCommands.ts
@@ -1,6 +1,7 @@
 import { Collection } from 'discord.js';
 import { Command } from '../lib/command';
 import { Status } from '../commands/status';
+import { Account } from '../commands/account';
 
 export type CommandCollection = Collection<string, Command>;
 
@@ -8,9 +9,10 @@ export default async function loadCommands(): Promise<Collection<string, Command
   const commands = new Collection<string, Command>();
 
   const status = new Status();
+  const account = new Account();
 
   // Include all commands here
   commands.set(status.title, status);
-
+  commands.set(account.title, account);
   return commands;
 }
