7d86df863c106e1d066d468925ee6d159632b309
Cameron Steele <cam.steeleis@gmail.com>
2023-06-24 22:55:39 -0500

    Support menus

--- src/events/onInteractionCreate.ts
+++ src/events/onInteractionCreate.ts
@@ -1,10 +1,25 @@
-import { Interaction, CacheType } from 'discord.js';
+import { Interaction, CacheType, UserContextMenuCommandInteraction } from 'discord.js';
 import EventData from '../interfaces/EventData.interface';
 
 export default async function onInteractionCreate(
   interaction: Interaction<CacheType>,
   data: EventData,
 ) {
+  if (interaction.isContextMenuCommand()) {
+    const { menus } = data;
+    const menu = menus.get(interaction.commandName);
+    if (!menu) return;
+    try {
+      await interaction.deferReply({ ephemeral: menu.isEphemeral });
+      await menu.run(interaction as UserContextMenuCommandInteraction<CacheType>);
+    } catch (error) {
+      console.error(error);
+      await interaction.editReply({
+        content: 'There was an error while executing this command!',
+      });
+    }
+  }
+
   if (interaction.isCommand()) {
     const { commands } = data;
     const command = commands.get(interaction.commandName);
