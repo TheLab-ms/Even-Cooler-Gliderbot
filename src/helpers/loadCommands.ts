0a86b2fb1d238fccba6d62fa26d601f387b22f32
Cameron Steele <cam.steeleis@gmail.com>
2024-03-19 18:00:28 -0500

    Add the keyfob command

--- src/helpers/loadCommands.ts
+++ src/helpers/loadCommands.ts
@@ -1,11 +1,12 @@
 import { Collection } from 'discord.js';
 import { Status } from '../interactions/commands/status';
 import { Command } from '../interfaces/Commands';
+import { Keyfob } from '../interactions/commands/keyfob';
 
 export type CommandCollection = Collection<string, Command>;
 
 export default async function loadCommands(): Promise<CommandCollection> {
-  const allCommands = [new Status()];
+  const allCommands = [new Status(), new Keyfob()];
   const commands = new Collection<string, Command>();
 
   // Include all commands here
